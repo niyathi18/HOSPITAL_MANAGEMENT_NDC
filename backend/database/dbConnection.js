9f31ef568084c1f6e876fc3657c54b20e455be59
niyathi18 <niyathisurthineni1801@gmail.com>
2024-06-03 15:04:03 +0530

    chages2.0

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
     mongoose.connect(process.env.MONGO_URI,{
-        dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM_DEPLOYED"
+        dbName: "MERN_NANDAN_DENTAL"
     }).then(()=>{
         console.log("Connected to database!");
     }).catch(err=>{
